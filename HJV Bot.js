8eadff1688a213a3449fa4a6b8b6f771ffd31a23
Franta72 <karamelovnaCZ@gmail.com>
2016-03-18 20:48:27 +0100

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -1987,41 +1987,20 @@ alkoholCommand: {
                     }
                 }
             },
-afkCommand: {
+
+            afkCommand: {
                 command: 'afk',
                 rank: 'user',
-                type: 'startsWith',
-                getafk: function (chat) {
-                    var c = Math.floor(Math.random() * basicBot.chat.afk.length);
-                    return basicBot.chat.afk[c];
-                },
+                type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        var msg = chat.message;
-
-                        var space = msg.indexOf(' ');
-                        if (space === -1) {
-                            API.sendChat(basicBot.chat.giveafk);
-                            return false;
-                        }
-                        else {
-                            var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
-                            if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nouserafk, {name: name}));
-                            }
-                            else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfafk, {name: name}));
-                            }
-                            else {
-                                return API.sendChat(subChat(basicBot.chat.afk, {nameto: user.username, namefrom: chat.un, AFK: this.getafk()}));
-                            }
-                        }
+                        API.sendChat(basicBot.chat.afk)
                     }
                 }
             },
+
 zpetCommand: {
                 command: 'zpet',
                 rank: 'user',
