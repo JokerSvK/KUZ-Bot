1e602ace7cc46a8e53c170bde1577e9e06d0b003
Franta72 <karamelovnaCZ@gmail.com>
2016-03-20 13:34:05 +0100

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -1180,8 +1180,11 @@
                         API.sendChat(subChat(basicBot.chat.roomadvertising, {name: chat.un}));
                         API.moderateDeleteChat(chat.cid);
                         return true;
-                    if var plugRoomLinkPatt = /(\bhttps?:\/\/(www.)?stg.plug\.dj[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                    else API.moderateBanUser(user.id, 1, API.BAN.PERMA);
+                    var plugRoomLinkPatt = /(\bhttps?:\/\/(www.)?stg.plug\.dj[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+                 if (plugRoomLinkPatt.exec(msg)) {
+                    if (perm === 0) {
+                    API.moderateBanUser(user.id, 1, API.BAN.PERMA);
+                     API.moderateDeleteChat(chat.cid);
                     return true;
                     } 
                 }
