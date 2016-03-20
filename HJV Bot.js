12bebddd354cbda4f7a9f799f0e633f85fa64176
Franta72 <karamelovnaCZ@gmail.com>
2016-03-20 16:00:18 +0100

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -1195,15 +1195,7 @@
                         
                     } 
                 }
-                var plugRoomLinkPatt = ('nigger','faggot','nigga','niqqa','motherfucker','modafocka'),
-                 if (plugRoomLinkPatt.exec(msg)) {
-                    if (perm === 0) {
-                        API.sendChat(subChat(basicBot.chat.urazky, {name: chat.un}));
-                        return true;
-                        
-                    } 
-                }
-                 
+                
                 if (msg.indexOf('http://adf.ly/') > -1) {
                     API.moderateDeleteChat(chat.cid);
                     API.sendChat(subChat(basicBot.chat.adfly, {name: chat.un}));
