49069a10a0b699f1979267193f408cfdc286bad9
Franta72 <karamelovnaCZ@gmail.com>
2016-03-22 15:59:18 +0100

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -2365,6 +2365,18 @@ sklepCommand: {
                     }
                 }
             },
+             kebabčičiCommand: {
+                command: 'kebabčiči',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.kebabčiči);
+                    }
+                }
+            },
             cycleCommand: {
                 command: 'cycle',
                 rank: 'manager',
