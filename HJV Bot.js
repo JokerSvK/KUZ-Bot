b7adc9d2d2863da1218f0e6ef89a37c17a1f2e8b
Franta72 <karamelovnaCZ@gmail.com>
2016-03-23 17:17:19 +0100

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -2233,6 +2233,41 @@ sklepCommand: {
                     }
                 }
             },
+            sluzbyCommand: {
+                command: ['sluzby', 'service'],
+                rank: 'user',
+                type: 'startsWith',
+                getsluzby: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.sluzby.length);
+                    return basicBot.chat.sluzby[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.givesluzby);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousersluzby, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfsluzby, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.sluzby, {nameto: user.username, namefrom: chat.un, SLUZBY: this.getsluzby()}));
+                            }
+                        }
+                    }
+                }
+            },
             prikazyCommand: {
                 command: 'prikazy',
                 rank: 'user',
