424dcd531bc9112e46e7b0e979642d29eabdd4ec
Franta72 <karamelovnaCZ@gmail.com>
2016-06-04 09:55:00 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -37,6 +37,19 @@
                 }
               }
             };
+            bot.commands.odmenaCommand = {
+            command: 'soutez',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("/me Probíhá u nás soutěž - kdo nasbírá 500 žetonů na automatu, bude posunut na první příčku ve frontě. Akce neplatí pro staff. Soutěž platí do odvolání.");
+
+                }
+              }
+            };
 // !cleartokens
         bot.commands.cleartokensCommand = {
             command: 'resetzetony',  //The command to be called. With the standard command literal this would be: !cleartokens
