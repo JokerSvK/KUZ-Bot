9a10e0e6a886d12152a5d404cd2fe8c54665ea12
Franta72 <karamelovnaCZ@gmail.com>
2016-06-05 21:00:56 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -129,7 +129,7 @@
                     localStorage.setItem("Dandeen.ka*", "500");
                     localStorage.setItem("BroPlaysKevinCZE", "500");
                     localStorage.setItem("Enciánová Predkožka", "2242");
-                    localStorage.setItem("GΣИΣRΔTIOИ XIII.", "280");
+                    localStorage.setItem("GΣИΣRΔTIOИ XIII.", "364");
                     API.sendChat("/me Proběhlo resetování žetonů!");
                 }
             }
