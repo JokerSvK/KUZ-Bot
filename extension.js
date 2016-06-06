f99d33c348f2c313e8a0050db7a2d9bf88843fb7
Franta72 <karamelovnaCZ@gmail.com>
2016-06-06 17:08:45 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -100,7 +100,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                  API.sendChat("/me 1. Mark Valentine: 168035 bodů, 2. Uszi 19050 bodů, 3. Predkozka: 17325 bodů");
+                  API.sendChat("/me 1. Mark Valentine: 168035 bodů, 2. Predkozka: 20000 bodů, 3. Uszi 19050 bodů");
 
                 }
               }
