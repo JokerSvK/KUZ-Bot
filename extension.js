c50245704ca460826b82038a84bd8079c3f71fae
Franta72 <karamelovnaCZ@gmail.com>
2016-06-10 15:38:17 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -307,8 +307,8 @@
                 winnings = Math.round(bet * (.40 * outcome2[1]));
                 
             }
-            else if (outcome2[0] == outcome4[0] && outcome3[0] != outcome1[0]) {
-                winnings = Math.round(bet * (.40 * outcome2[1]));
+            else if (outcome1[0] == outcome3[0] && outcome4[0] != outcome2[0]) {
+                winnings = Math.round(bet * (.45 * outcome1[1]));
             
             }
             else{
