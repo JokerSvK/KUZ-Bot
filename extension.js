f6bad9c92d58728df70e4b9155144db3df32b67a
Franta72 <karamelovnaCZ@gmail.com>
2016-06-11 00:07:00 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -310,7 +310,7 @@
                
             }
             else if (outcome1[0] == outcome4[0] && outcome3[0] != outcome2[0]) {
-                winnings = Math.round(bet * (.45 * outcome1[1]));
+                winnings = Math.round(bet * (.40 * outcome1[1]));
                 
             }
             else if (outcome1[0] == outcome5[0] && outcome1[0] != outcome2[0]) {
