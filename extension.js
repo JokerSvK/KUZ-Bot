5bf0505e9726d98819adfa98a22e7faf1fe3dfd9
Franta72 <karamelovnaCZ@gmail.com>
2016-06-11 23:39:47 +0200

    Grab hype - nový příkaz

--- extension.js
+++ extension.js
@@ -92,15 +92,15 @@
                 }
             }
         };
-            bot.commands.vysledkyCommand = {
-            command: 'vysledky',  // The command to be called. With the standard command literal this would be: !bacon
+            bot.commands.gdgrabCommand = {
+            command: 'gdgrab',  // The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', // Minimum user permission to use the command
             type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
               functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                  API.sendChat("/me Červnovou soutěž vyhrál hráč Mark Valentine.");
+                  API.sendChat("/me  GeeDee grab hype! Raduj se! Stal/a jsi se obětí vzáceného stisknutí tohoto tlačítka..");
 
                 }
               }
