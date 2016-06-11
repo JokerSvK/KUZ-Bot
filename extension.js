ff78a22707c128c3edc73d74a2f2b5aa932226ab
Franta72 <karamelovnaCZ@gmail.com>
2016-06-11 18:32:27 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -291,41 +291,41 @@
              if (outcome1[0] == outcome2[0] && outcome1[0] == outcome3[0]) {
                 winnings = Math.round(bet * outcome1[1]);
             }
-            else if (outcome1[0] == outcome2[0] && outcome1[0] != outcome3[0]) {
+            else if (outcome1[0] == outcome2[0] && outcome1[0] != outcome3[0] & outcome4[0] & outcome5[0] && outcome6[0]) {
                 winnings = Math.round(bet * (.45 * outcome1[1]));
             }
-            else if (outcome1[0] == outcome3[0] && outcome1[0] != outcome2[0]) {
+            else if (outcome1[0] == outcome3[0] && outcome1[0] != outcome2[0] & outcome4[0] & outcome5[0] && outcome6[0]) {
                 winnings = Math.round(bet * (.5 * outcome1[1]));
             }
-            else if (outcome2[0] == outcome3[0] && outcome2[0] != outcome1[0]) {
+            else if (outcome2[0] == outcome3[0] && outcome2[0] != outcome1[0] & outcome4[0] & outcome5[0] && outcome6[0]) {
                 winnings = Math.round(bet * (.40 * outcome2[1]));
                 
             }
-            else if (outcome1[0] == outcome5[0] && outcome4[0] != outcome6[0]) {
+            else if (outcome1[0] == outcome5[0] && outcome4[0] != outcome1[0] & outcome2[0] & outcome3[0] && outcome6[0]) {
                 winnings = Math.round(bet * (.45 * outcome1[1]));
             }
-            else if (outcome1[0] == outcome6[0] && outcome4[0] != outcome5[0]) {
+            else if (outcome1[0] == outcome6[0] && outcome4[0] != outcome1[0] & outcome2[0] & outcome3[0] && outcome5[0]) {
                 winnings = Math.round(bet * (.45 * outcome1[1]));
             }
-            else if (outcome2[0] == outcome6[0] && outcome5[0] != outcome4[0]) {
-                winnings = Math.round(bet * (.40 * outcome2[1]));
-                
-            }
-            else if (outcome1[0] == outcome4[0] && outcome3[0] != outcome5[0] & outcome6[0] & outcome2[0] && outcome1[0]) {
-                winnings = Math.round(bet * (.45 * outcome1[1]));
-            }
-            else if (outcome1[0] == outcome5[0] && outcome3[0] != outcome4[0]) {
-                winnings = Math.round(bet * (.5 * outcome1[1]));
-            }
-            else if (outcome2[0] == outcome5[0] && outcome2[0] != outcome4[0]) {
-                winnings = Math.round(bet * (.40 * outcome2[1]));
-                
-            }
-            else if (outcome2[0] == outcome5[0] && outcome1[0] != outcome3[0]) {
+            else if (outcome2[0] == outcome6[0] && outcome5[0] != outcome1[0] & outcome2[0] & outcome3[0] && outcome4[0]) {
                 winnings = Math.round(bet * (.45 * outcome2[1]));
                 
             }
-            else if (outcome2[0] == outcome4[0] && outcome2[0] != outcome3[0]) {
+            else if (outcome1[0] == outcome4[0] && outcome3[0] != outcome1[0] & outcome2[0] & outcome5[0] && outcome6[0]) {
+                winnings = Math.round(bet * (.45 * outcome1[1]));
+            }
+            else if (outcome1[0] == outcome5[0] && outcome3[0] != outcome1[0] & outcome2[0] & outcome4[0] && outcome6[0]) {
+                winnings = Math.round(bet * (.45 * outcome1[1]));
+            }
+            else if (outcome2[0] == outcome5[0] && outcome2[0] != outcome1[0] & outcome3[0] & outcome4[0] && outcome6[0]) {
+                winnings = Math.round(bet * (.40 * outcome2[1]));
+                
+            }
+            else if (outcome2[0] == outcome5[0] && outcome1[0] != outcome2[0] & outcome3[0] & outcome4[0] && outcome6[0]) {
+                winnings = Math.round(bet * (.45 * outcome2[1]));
+                
+            }
+            else if (outcome2[0] == outcome4[0] && outcome2[0] != outcome1[0] & outcome3[0] & outcome5[0] && outcome6[0]) {
                 winnings = Math.round(bet * (.45 * outcome2[1]));
             }
             else{
