bc43b7d24245da4b1b82feaac8455abea4171f06
Franta72 <karamelovnaCZ@gmail.com>
2016-06-12 20:40:18 +0200

    Update HJV Bot.js

--- HJV Bot.js
+++ HJV Bot.js
@@ -1302,13 +1302,10 @@
                 basicBot.room.roomstats.chatmessages++;
             },
             spam: [
-                'hueh', 'hu3', 'brbr', 'heu', 'brbr', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
-                'zueria', 'aehoo', 'aheu', 'alguem', 'algum', 'brazil', 'zoeira', 'fuckadmins', 'affff', 'vaisefoder', 'huenaarea',
-                'hitler', 'ashua', 'ahsu', 'ashau', 'lulz', 'huehue', 'hue', 'huehuehue', 'merda', 'pqp', 'puta', 'mulher', 'pula', 'retarda', 'caralho', 'filha', 'ppk',
-                'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda'
+                'kokot', 'píča', 'piča', 'pica', 'debil', 'magor', 'ichtyl', 'zmrd', 'kurva', 'děvka', 'fuck', 'bitch', 'curak', 'čůrak', 'čurak', 'retard'
             ],
             curses: [
-                'kokot', 'píča', 'piča', 'pica', 'debil', 'magor', 'ichtyl', 'zmrd', 'kurva', 'děvka', 'fuck', 'bitch', 'curak', 'čůrak', 'čurak', 'retard'
+                'spam'
             ]
         },
         connectAPI: function () {
