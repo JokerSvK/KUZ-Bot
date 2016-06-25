f2c79e54b6397584985314ab87ad75f632d6d7f2
Franta72 <karamelovnaCZ@gmail.com>
2016-06-25 23:21:02 +0200

    Update KUZ Bot.js

--- KUZ Bot.js
+++ KUZ Bot.js
@@ -2426,14 +2426,14 @@ sklepCommand: {
                 }
             },
             germCommand: {
-                command: 'geedee',
+                command: 'germ',
                 rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(basicBot.chat.geedee);
+                        API.sendChat(basicBot.chat.germ);
                     }
                 }
             },
